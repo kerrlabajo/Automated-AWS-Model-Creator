9e692100a1a667b8de19ea21e582cdb27f0b3d0e
kerrlabajo <labajokerr5@gmail.com>
2023-12-04 19:11:49 +0800

    Add build and push shell script

---
+++ training-container/scripts/build_and_push.sh
@@ -0,0 +1,22 @@
+source .env
+
+# Retrieve from developer/user's .env file
+ACCOUNT_ID=${ACCOUNT_ID}
+REGION=${REGION}
+REPO_NAME=${REPO_NAME}
+IMAGE_TAG=${IMAGE_TAG}
+
+# Increment the image tag
+IMAGE_TAG=$((IMAGE_TAG + 1))
+echo "IMAGE_TAG=$IMAGE_TAG" >> .env
+IMAGE_TAG=${IMAGE_TAG}
+
+docker build -f ../docker/Dockerfile -t $REPO_NAME ../docker
+
+docker tag $REPO_NAME $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$REPO_NAME:$IMAGE_TAG
+
+$(aws ecr get-login --no-include-email --registry-ids $ACCOUNT_ID)
+
+aws ecr describe-repositories --repository-names $REPO_NAME || aws ecr create-repository --repository-name $REPO_NAME
+
+docker push $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$REPO_NAME:$IMAGE_TAG
