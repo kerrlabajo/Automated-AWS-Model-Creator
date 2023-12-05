c56e99e8a970a8d632c23e0cf56f32d33f82c09c
kerrlabajo <labajokerr5@gmail.com>
2023-12-05 21:06:56 +0800

    Implement action timer and logger

--- training-container/docker/train.py
+++ training-container/docker/train.py
@@ -2,6 +2,7 @@ import os
 import subprocess
 import boto3
 import yaml
+import time
 from dotenv import load_dotenv
 
 # Set the environment variable to avoid multithreading conflicts
@@ -10,10 +11,16 @@ os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 # Load environment variables from .env file
 load_dotenv()
 
-# Create an s3 resource object
-s3 = boto3.resource('s3', 
-     aws_access_key_id=os.getenv('ACCESS_KEY_ID'), 
-     aws_secret_access_key=os.getenv('SECRET_ACCESS_KEY'))
+# Define a method that times the execution of a function
+def time_action(method):
+  def logger(*args, **kw):
+      print('\033[1m' + '\nStarting %r...' % (method.__name__) + '\033[0m')
+      ts = time.time()
+      result = method(*args, **kw)
+      te = time.time()
+      print('\033[4m' + '%r completed in ' % (method.__name__) + '\033[92m%2.2f' % (te-ts) + '\033[4m seconds \033[0m\n')
+      return result
+  return logger
 
 # Download the file
 s3.Bucket(os.getenv('S3_BUCKET_NAME')).download_file(os.getenv('S3_FILE_KEY'), 'dataset.zip')
