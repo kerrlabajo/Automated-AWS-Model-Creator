d763a58125be95094d3dd87c97c039058a066f95
kerrlabajo <labajokerr5@gmail.com>
2023-12-06 00:24:00 +0800

    Add permission handler upon finding/creating repo

--- training-container/scripts/build_and_push.sh
+++ training-container/scripts/build_and_push.sh
@@ -17,6 +17,6 @@ docker tag $REPO_NAME $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$REPO_NAME:$IMAG
 
 aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com
 
-aws ecr describe-repositories --repository-names $REPO_NAME || aws ecr create-repository --repository-name $REPO_NAME
+aws ecr describe-repositories --repository-names $REPO_NAME > /dev/null 2>&1 || aws ecr create-repository --repository-name $REPO_NAME > /dev/null 2>&1
 
 docker push $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$REPO_NAME:$IMAGE_TAG
