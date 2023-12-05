e32547b8199ff98d58dcab24ee1c21f97d6f31c4
kerrlabajo <labajokerr5@gmail.com>
2023-12-06 00:22:28 +0800

    Use get-login-password and docker login for auth

--- training-container/scripts/build_and_push.sh
+++ training-container/scripts/build_and_push.sh
@@ -15,7 +15,7 @@ docker build -f ../docker/Dockerfile -t $REPO_NAME ../docker
 
 docker tag $REPO_NAME $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$REPO_NAME:$IMAGE_TAG
 
-$(aws ecr get-login --no-include-email --registry-ids $ACCOUNT_ID)
+aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com
 
 aws ecr describe-repositories --repository-names $REPO_NAME || aws ecr create-repository --repository-name $REPO_NAME
 
