372cfb6f218b059279c65c5e7f217356e2086107
kerrlabajo <labajokerr5@gmail.com>
2024-01-30 01:57:04 +0800

    Re-add previous params and push via private repo again

--- training-container/scripts/build_and_push.sh
+++ training-container/scripts/build_and_push.sh
@@ -3,13 +3,14 @@
 # TODO: Set `build_and_push.sh`` to receive parameters instead of environment variables.
 
 # Retrieve from developer/user's .env file
-REGISTRY_ALIAS=$1
-REPO_NAME=$2
-IMAGE_TAG=$3
+ACCOUNT_ID=$1
+REGION=$2
+REPO_NAME=$3
+IMAGE_TAG=$4
 
 # Check if all arguments are provided
-if [ -z "$REGISTRY_ALIAS" ] || [ -z "$REPO_NAME" ] || [ -z "$IMAGE_TAG" ]; then
-    echo "Usage: $0 <region> <registry_alias> <repo_name> <image_tag>"
+if [ -z "$ACCOUNT_ID" ] || [ -z "$REGION" ] || [ -z "$REPO_NAME" ] || [ -z "$IMAGE_TAG" ]; then
+    echo "Usage: $0 <account_id> <region> <repo_name> <image_tag>"
     exit 1
 fi
 
@@ -19,10 +20,10 @@ docker build -f ../docker/yolov5/Dockerfile -t $REPO_NAME ../docker/yolov5
 
 docker rmi ubuntu-cuda
 
-docker tag $REPO_NAME public.ecr.aws/$REGISTRY_ALIAS/$REPO_NAME:$IMAGE_TAG
+docker tag $REPO_NAME $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$REPO_NAME:$IMAGE_TAG
 
-aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws
+aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com
 
-aws ecr-public describe-repositories --repository-names $REPO_NAME > /dev/null 2>&1 || aws ecr-public create-repository --repository-name $REPO_NAME > /dev/null 2>&1
+aws ecr describe-repositories --repository-names $REPO_NAME > /dev/null 2>&1 || aws ecr create-repository --repository-name $REPO_NAME > /dev/null 2>&1
 
-docker push public.ecr.aws/$REGISTRY_ALIAS/$REPO_NAME:$IMAGE_TAG
+docker push $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$REPO_NAME:$IMAGE_TAG
