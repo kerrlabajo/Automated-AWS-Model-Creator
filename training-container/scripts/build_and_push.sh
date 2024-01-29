ba764da0eb39e8d7aacc6f6129803777b96dddbc
kerrlabajo <labajokerr5@gmail.com>
2024-01-29 18:41:47 +0800

    Remove unused account id and region
    This is because registry alias will be used instead in order to utilize PUBLIC ECR

--- training-container/scripts/build_and_push.sh
+++ training-container/scripts/build_and_push.sh
@@ -3,15 +3,13 @@
 # TODO: Set `build_and_push.sh`` to receive parameters instead of environment variables.
 
 # Retrieve from developer/user's .env file
-ACCOUNT_ID=$1
-REGION=$2
-REGISTRY_ALIAS=$3
-REPO_NAME=$4
-IMAGE_TAG=$5
+REGISTRY_ALIAS=$1
+REPO_NAME=$2
+IMAGE_TAG=$3
 
 # Check if all arguments are provided
-if [ -z "$ACCOUNT_ID" ] || [ -z "$REGION" ] || [ -z "$REGISTRY_ALIAS" ] || [ -z "$REPO_NAME" ] || [ -z "$IMAGE_TAG" ]; then
-    echo "Usage: $0 <account_id> <region> <registry_alias> <repo_name> <image_tag>"
+if [ -z "$REGISTRY_ALIAS" ] || [ -z "$REPO_NAME" ] || [ -z "$IMAGE_TAG" ]; then
+    echo "Usage: $0 <region> <registry_alias> <repo_name> <image_tag>"
     exit 1
 fi
 
@@ -23,7 +21,7 @@ docker rmi ubuntu-cuda
 
 docker tag $REPO_NAME public.ecr.aws/$REGISTRY_ALIAS/$REPO_NAME:$IMAGE_TAG
 
-aws ecr-public get-login-password --region $REGION | docker login --username AWS --password-stdin public.ecr.aws
+aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws
 
 aws ecr-public describe-repositories --repository-names $REPO_NAME > /dev/null 2>&1 || aws ecr-public create-repository --repository-name $REPO_NAME > /dev/null 2>&1
 
