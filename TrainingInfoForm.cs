7fdc6c13cd6de2550e63808590c6e9a1b234d85c
Jhonray Acojedo <jhonray.acojedo@cit.edu>
2024-01-30 22:19:48 +0800

    Add training info form

---
+++ TrainingInfoForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LSC_Trainer
+{
+    public partial class TrainingInfoForm : Form
+    {
+        public TrainingInfoForm()
+        {
+            InitializeComponent();
+        }
+
+        private void TrainingInfoForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
