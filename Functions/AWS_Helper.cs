db394fe1c665a9a5f8a1f04d6baa52362983e020
Ra-Jay <rjyansonx@gmail.com>
2024-02-01 22:56:42 +0800

    create a helper method that fetch available models

--- Functions/AWS_Helper.cs
+++ Functions/AWS_Helper.cs
@@ -269,7 +269,6 @@ namespace LSC_Trainer.Functions
             }
         }
 
-
         public static async Task DownloadObjects(AmazonS3Client s3Client, string bucketName, string objectKey, string localFilePath)
         {
             try
@@ -318,6 +317,30 @@ namespace LSC_Trainer.Functions
             catch (AmazonS3Exception e)
             {
                 Console.WriteLine("Error downloading file: " + e.Message);
+            }
+        }
+
+        public static async Task<List<string>> GetModelListFromS3(AmazonS3Client s3Client, string bucketName)
+        {
+            try
+            {
+                var response = await s3Client.ListObjectsV2Async(new ListObjectsV2Request
+                {
+                    BucketName = bucketName,
+                     Prefix = "models"
+                });
+
+                return response.S3Objects.Select(o => o.Key).ToList();
+            }
+            catch (AmazonS3Exception e)
+            {
+                Console.WriteLine("Error retrieving list from S3: " + e.Message);
+                return null;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+                return null;
             }
         }
 
