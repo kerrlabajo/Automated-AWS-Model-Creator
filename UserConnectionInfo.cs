2b356b9f4258b7727bcf697efb2e5d0acfcdef7f
Ra-Jay <rjyansonx@gmail.com>
2024-02-19 00:22:59 +0800

    Create a singleton class for storing user creds

---
+++ UserConnectionInfo.cs
@@ -0,0 +1,16 @@
+﻿namespace LSC_Trainer
+{
+    class UserConnectionInfo
+    {
+        public static string AccountId { get; set; }
+        public static string AccessKey { get; set; }
+        public static string SecretKey { get; set; }
+        public static string Region { get; set; }
+        public static string RoleArn { get; set; }
+        public static string EcrUri { get; set; }
+        public static string SagemakerBucket { get; set; }
+        public static string DefaultDatasetURI { get; set; }
+        public static string CustomUploadsURI { get; set; }
+        public static string DestinationURI { get; set; }
+    }
+}
