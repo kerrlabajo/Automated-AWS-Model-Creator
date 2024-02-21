0c96cd5f245e32d2d71b97bfdf3ee342ba163127
kerrlabajo <labajokerr5@gmail.com>
2024-02-21 23:10:41 +0800

    Set proper label and textbox naming as well as organize tab indeces

--- CreateConnectionForm.Designer.cs
+++ CreateConnectionForm.Designer.cs
@@ -29,49 +29,49 @@ namespace LSC_Trainer
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.button1 = new System.Windows.Forms.Button();
+            this.createConnectionPanel = new System.Windows.Forms.Panel();
+            this.btnConnect = new System.Windows.Forms.Button();
             this.regionDropdown = new System.Windows.Forms.ComboBox();
             this.roleARN = new System.Windows.Forms.TextBox();
             this.secretKeyID = new System.Windows.Forms.TextBox();
             this.accessKeyID = new System.Windows.Forms.TextBox();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
+            this.roleARNLabel = new System.Windows.Forms.Label();
+            this.regionLabel = new System.Windows.Forms.Label();
+            this.secretKeyIdLabel = new System.Windows.Forms.Label();
+            this.accessKeyIdLabel = new System.Windows.Forms.Label();
+            this.accountIdLabel = new System.Windows.Forms.Label();
             this.accountID = new System.Windows.Forms.TextBox();
-            this.panel1.SuspendLayout();
+            this.createConnectionPanel.SuspendLayout();
             this.SuspendLayout();
             // 
-            // panel1
+            // createConnectionPanel
             // 
-            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel1.Controls.Add(this.button1);
-            this.panel1.Controls.Add(this.regionDropdown);
-            this.panel1.Controls.Add(this.roleARN);
-            this.panel1.Controls.Add(this.secretKeyID);
-            this.panel1.Controls.Add(this.accessKeyID);
-            this.panel1.Controls.Add(this.label5);
-            this.panel1.Controls.Add(this.label4);
-            this.panel1.Controls.Add(this.label3);
-            this.panel1.Controls.Add(this.label2);
-            this.panel1.Controls.Add(this.label1);
-            this.panel1.Controls.Add(this.accountID);
-            this.panel1.Location = new System.Drawing.Point(60, 48);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(554, 355);
-            this.panel1.TabIndex = 5;
+            this.createConnectionPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.createConnectionPanel.Controls.Add(this.btnConnect);
+            this.createConnectionPanel.Controls.Add(this.regionDropdown);
+            this.createConnectionPanel.Controls.Add(this.roleARN);
+            this.createConnectionPanel.Controls.Add(this.secretKeyID);
+            this.createConnectionPanel.Controls.Add(this.accessKeyID);
+            this.createConnectionPanel.Controls.Add(this.roleARNLabel);
+            this.createConnectionPanel.Controls.Add(this.regionLabel);
+            this.createConnectionPanel.Controls.Add(this.secretKeyIdLabel);
+            this.createConnectionPanel.Controls.Add(this.accessKeyIdLabel);
+            this.createConnectionPanel.Controls.Add(this.accountIdLabel);
+            this.createConnectionPanel.Controls.Add(this.accountID);
+            this.createConnectionPanel.Location = new System.Drawing.Point(60, 48);
+            this.createConnectionPanel.Name = "createConnectionPanel";
+            this.createConnectionPanel.Size = new System.Drawing.Size(554, 355);
+            this.createConnectionPanel.TabIndex = 5;
             // 
-            // button1
+            // btnConnect
             // 
-            this.button1.Location = new System.Drawing.Point(395, 298);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(104, 31);
-            this.button1.TabIndex = 11;
-            this.button1.Text = "Connect";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btnConnect.Location = new System.Drawing.Point(395, 298);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(104, 31);
+            this.btnConnect.TabIndex = 6;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.button1_Click);
             // 
             // regionDropdown
             // 
@@ -90,7 +90,7 @@ namespace LSC_Trainer
             this.regionDropdown.Location = new System.Drawing.Point(180, 171);
             this.regionDropdown.Name = "regionDropdown";
             this.regionDropdown.Size = new System.Drawing.Size(319, 24);
-            this.regionDropdown.TabIndex = 10;
+            this.regionDropdown.TabIndex = 4;
             this.regionDropdown.SelectedValueChanged += new System.EventHandler(this.comboBox1_SelectedValueChanged);
             // 
             // roleARN
@@ -98,14 +98,14 @@ namespace LSC_Trainer
             this.roleARN.Location = new System.Drawing.Point(180, 221);
             this.roleARN.Name = "roleARN";
             this.roleARN.Size = new System.Drawing.Size(319, 22);
-            this.roleARN.TabIndex = 9;
+            this.roleARN.TabIndex = 5;
             // 
             // secretKeyID
             // 
             this.secretKeyID.Location = new System.Drawing.Point(180, 121);
             this.secretKeyID.Name = "secretKeyID";
             this.secretKeyID.Size = new System.Drawing.Size(319, 22);
-            this.secretKeyID.TabIndex = 7;
+            this.secretKeyID.TabIndex = 3;
             this.secretKeyID.UseSystemPasswordChar = true;
             // 
             // accessKeyID
@@ -113,87 +113,87 @@ namespace LSC_Trainer
             this.accessKeyID.Location = new System.Drawing.Point(180, 80);
             this.accessKeyID.Name = "accessKeyID";
             this.accessKeyID.Size = new System.Drawing.Size(319, 22);
-            this.accessKeyID.TabIndex = 6;
+            this.accessKeyID.TabIndex = 2;
             this.accessKeyID.UseSystemPasswordChar = true;
             // 
-            // label5
+            // roleARNLabel
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(36, 221);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(70, 17);
-            this.label5.TabIndex = 5;
-            this.label5.Text = "Role ARN";
+            this.roleARNLabel.AutoSize = true;
+            this.roleARNLabel.Location = new System.Drawing.Point(36, 221);
+            this.roleARNLabel.Name = "roleARNLabel";
+            this.roleARNLabel.Size = new System.Drawing.Size(68, 16);
+            this.roleARNLabel.TabIndex = 5;
+            this.roleARNLabel.Text = "Role ARN";
             // 
-            // label4
+            // regionLabel
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(36, 171);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(53, 17);
-            this.label4.TabIndex = 4;
-            this.label4.Text = "Region";
+            this.regionLabel.AutoSize = true;
+            this.regionLabel.Location = new System.Drawing.Point(36, 171);
+            this.regionLabel.Name = "regionLabel";
+            this.regionLabel.Size = new System.Drawing.Size(51, 16);
+            this.regionLabel.TabIndex = 4;
+            this.regionLabel.Text = "Region";
             // 
-            // label3
+            // secretKeyIdLabel
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(36, 124);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(94, 17);
-            this.label3.TabIndex = 3;
-            this.label3.Text = "Secret Key ID";
+            this.secretKeyIdLabel.AutoSize = true;
+            this.secretKeyIdLabel.Location = new System.Drawing.Point(36, 124);
+            this.secretKeyIdLabel.Name = "secretKeyIdLabel";
+            this.secretKeyIdLabel.Size = new System.Drawing.Size(88, 16);
+            this.secretKeyIdLabel.TabIndex = 3;
+            this.secretKeyIdLabel.Text = "Secret Key ID";
             // 
-            // label2
+            // accessKeyIdLabel
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(36, 80);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(98, 17);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Access Key ID";
+            this.accessKeyIdLabel.AutoSize = true;
+            this.accessKeyIdLabel.Location = new System.Drawing.Point(36, 80);
+            this.accessKeyIdLabel.Name = "accessKeyIdLabel";
+            this.accessKeyIdLabel.Size = new System.Drawing.Size(94, 16);
+            this.accessKeyIdLabel.TabIndex = 2;
+            this.accessKeyIdLabel.Text = "Access Key ID";
             // 
-            // label1
+            // accountIdLabel
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(36, 41);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(76, 17);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Account ID";
+            this.accountIdLabel.AutoSize = true;
+            this.accountIdLabel.Location = new System.Drawing.Point(36, 41);
+            this.accountIdLabel.Name = "accountIdLabel";
+            this.accountIdLabel.Size = new System.Drawing.Size(71, 16);
+            this.accountIdLabel.TabIndex = 1;
+            this.accountIdLabel.Text = "Account ID";
             // 
             // accountID
             // 
             this.accountID.Location = new System.Drawing.Point(180, 38);
             this.accountID.Name = "accountID";
             this.accountID.Size = new System.Drawing.Size(319, 22);
-            this.accountID.TabIndex = 0;
+            this.accountID.TabIndex = 1;
             // 
             // CreateConnectionForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(684, 450);
-            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.createConnectionPanel);
             this.Name = "CreateConnectionForm";
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
+            this.createConnectionPanel.ResumeLayout(false);
+            this.createConnectionPanel.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel createConnectionPanel;
         private System.Windows.Forms.TextBox accountID;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label accessKeyIdLabel;
+        private System.Windows.Forms.Label accountIdLabel;
+        private System.Windows.Forms.Label secretKeyIdLabel;
+        private System.Windows.Forms.Label regionLabel;
         private System.Windows.Forms.ComboBox regionDropdown;
         private System.Windows.Forms.TextBox roleARN;
         private System.Windows.Forms.TextBox secretKeyID;
         private System.Windows.Forms.TextBox accessKeyID;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label roleARNLabel;
+        private System.Windows.Forms.Button btnConnect;
     }
 }
