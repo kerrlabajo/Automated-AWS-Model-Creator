ef8cb2d5d16711f1e69f4c983d34a58a9bb8e1ff
kerrlabajo <labajokerr5@gmail.com>
2024-02-24 21:25:13 +0800

    Rename labels and button

--- EmployeeEcrUriInputForm.Designer.cs
+++ EmployeeEcrUriInputForm.Designer.cs
@@ -30,8 +30,8 @@ namespace LSC_Trainer
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.continueBtn = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
+            this.btnContinue = new System.Windows.Forms.Button();
+            this.ecrURILabel = new System.Windows.Forms.Label();
             this.ecrUri = new System.Windows.Forms.TextBox();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
@@ -39,39 +39,39 @@ namespace LSC_Trainer
             // panel1
             // 
             this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel1.Controls.Add(this.continueBtn);
-            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.btnContinue);
+            this.panel1.Controls.Add(this.ecrURILabel);
             this.panel1.Controls.Add(this.ecrUri);
             this.panel1.Location = new System.Drawing.Point(65, 39);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(554, 166);
             this.panel1.TabIndex = 6;
             // 
-            // continueBtn
+            // btnContinue
             // 
-            this.continueBtn.Location = new System.Drawing.Point(395, 92);
-            this.continueBtn.Name = "continueBtn";
-            this.continueBtn.Size = new System.Drawing.Size(104, 31);
-            this.continueBtn.TabIndex = 11;
-            this.continueBtn.Text = "Continue";
-            this.continueBtn.UseVisualStyleBackColor = true;
-            this.continueBtn.Click += new System.EventHandler(this.continueBtn_Click);
+            this.btnContinue.Location = new System.Drawing.Point(395, 92);
+            this.btnContinue.Name = "btnContinue";
+            this.btnContinue.Size = new System.Drawing.Size(104, 31);
+            this.btnContinue.TabIndex = 11;
+            this.btnContinue.Text = "Continue";
+            this.btnContinue.UseVisualStyleBackColor = true;
+            this.btnContinue.Click += new System.EventHandler(this.continueBtn_Click);
             // 
-            // label1
+            // ecrURILabel
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(36, 41);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(63, 17);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "ECR URI";
+            this.ecrURILabel.AutoSize = true;
+            this.ecrURILabel.Location = new System.Drawing.Point(36, 41);
+            this.ecrURILabel.Name = "ecrURILabel";
+            this.ecrURILabel.Size = new System.Drawing.Size(61, 16);
+            this.ecrURILabel.TabIndex = 1;
+            this.ecrURILabel.Text = "ECR URI";
             // 
             // ecrUri
             // 
             this.ecrUri.Location = new System.Drawing.Point(180, 38);
             this.ecrUri.Name = "ecrUri";
             this.ecrUri.Size = new System.Drawing.Size(319, 22);
-            this.ecrUri.TabIndex = 0;
+            this.ecrUri.TabIndex = 1;
             // 
             // EmployeeEcrUriInputForm
             // 
@@ -90,8 +90,8 @@ namespace LSC_Trainer
         #endregion
 
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Button continueBtn;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnContinue;
+        private System.Windows.Forms.Label ecrURILabel;
         private System.Windows.Forms.TextBox ecrUri;
     }
 }
