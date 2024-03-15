93b0231071eba541e20d8c60127fc290cd7cc50b
kerrlabajo <labajokerr5@gmail.com>
2024-03-15 17:18:31 +0800

    Set another attempt to look for environment variables in container

--- training-container/docker/yolov5/network_finder.py
+++ training-container/docker/yolov5/network_finder.py
@@ -1,10 +1,5 @@
 import os
 import json
+import subprocess
 
-master_address = json.loads(os.environ['SM_HOSTS'])[0]
-num_gpus = os.environ['SM_NUM_GPUS']
-training_env = json.loads(os.environ['SM_TRAINING_ENV'])
-
-print(master_address)
-print(num_gpus)
-print(training_env)
+subprocess.run("printenv")
