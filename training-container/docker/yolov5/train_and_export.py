d5ec75d233139367030ce848185dca2e12cfab4b
kerrlabajo <labajokerr5@gmail.com>
2024-03-27 20:08:12 +0800

    Get gpu info using `nvidia-smi`

--- training-container/docker/yolov5/train_and_export.py
+++ training-container/docker/yolov5/train_and_export.py
@@ -108,6 +108,9 @@ def main():
     
     run_script(resource_config_args)
     
+    gpu_info = subprocess.check_output('nvidia-smi', shell=True).decode('utf-8')
+    print(gpu_info)
+    
     run_script(converter_args) if args.hyp == "Custom" else None
         
     if int(args.nnodes) > 1:
