47e5cdac48155987d7eea09c12ccfaacdc5328cd
Ra-Jay <rjyansonx@gmail.com>
2024-03-30 20:45:14 +0800

    add: handle error in docker image building

--- training-container/scripts/build_and_push.sh
+++ training-container/scripts/build_and_push.sh
@@ -10,9 +10,23 @@ if [ -z "$ACCOUNT_ID" ] || [ -z "$REGION" ] || [ -z "$REPO_NAME" ] || [ -z "$IMA
     exit 1
 fi
 
+# Check build for errors
+build_docker_image() {
+    # $1 - Dockerfile path
+    # $2 - Image tag
+    # $3 - Context
+    echo "Building Docker image: $2"
+    docker build -f $1 -t $2 $3 || {
+        echo "Error building Docker image: $2"
+        exit 1
+    }
+}
+
 # Build ubuntu-cuda and yolov5 images
-docker build -f ../docker/ubuntu-cuda/Dockerfile -t ubuntu-cuda ../docker/ubuntu-cuda
-docker build -f ../docker/yolov5/Dockerfile -t $REPO_NAME ../docker/yolov5
+build_docker_image ../docker/ubuntu-cuda/Dockerfile ubuntu-cuda ../docker/ubuntu-cuda
+build_docker_image ../docker/yolov5/Dockerfile $REPO_NAME ../docker/yolov5
+# docker build -f ../docker/ubuntu-cuda/Dockerfile -t ubuntu-cuda ../docker/ubuntu-cuda
+# docker build -f ../docker/yolov5/Dockerfile -t $REPO_NAME ../docker/yolov5
 
 # Tag image according to user's id and region
 docker tag $REPO_NAME $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$REPO_NAME:$IMAGE_TAG
