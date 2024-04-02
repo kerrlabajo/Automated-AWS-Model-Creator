82b647ef84bc795252e3f839929a6c8ba2bf8a0f
Jhonray Acojedo <jhonray.acojedo@cit.edu>
2024-04-02 17:42:55 +0800

    Revert unit test to xunit

--- LSC-TrainerUnitTest/UnitTest1.cs
+++ LSC-TrainerUnitTest/UnitTest1.cs
@@ -1,14 +1,11 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
-
 namespace LSC_TrainerUnitTest
 {
-    [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        [Fact]
+        public void Test1()
         {
+
         }
     }
 }
