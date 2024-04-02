8fed71ec734c0814f308311db8147e7626a735b6
Jhonray Acojedo <jhonray.acojedo@cit.edu>
2024-04-02 15:24:05 +0800

    Add 4.8 framework unit test

---
+++ Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Test")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3a0bd8a8-ca19-4c02-ab5c-0abc0dfc5708")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Test/Utils.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test
+{
+    class Utils
+    {
+        public static Control GetControlNamed(Control ParentControl, string name, bool searchAllChildren)
+        {
+            Control control = null;
+            Control[] allControls = ParentControl.Controls.Find(name, searchAllChildren);
+
+            if (allControls.Length > 0)
+            {
+                control = allControls[0];
+            }
+
+            return control;
+        }
+    }
+}
