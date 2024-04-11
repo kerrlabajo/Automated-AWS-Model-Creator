d8a15b9c89e7970973e2ff7b2a443da5116d5ea8
kerrlabajo <labajokerr5@gmail.com>
2024-04-11 12:18:20 +0800

    Remove unused and use appropriate variables

--- training-container/ubuntu-cuda-supported-yolov5-training/train_and_export.py
+++ training-container/ubuntu-cuda-supported-yolov5-training/train_and_export.py
@@ -5,7 +5,6 @@ import json
 import os
 import sys
 import traceback
-import socket
     
 def get_node_rank():
     with open('/opt/ml/input/config/resourceconfig.json') as f:
@@ -76,8 +75,6 @@ def main():
     current_host, node_rank = get_node_rank()
     master_host = 'algo-1'
     master_port = "29500"
-    fetched_master_ip = socket.gethostbyname(master_host)
-    fetched_local_ip = socket.gethostbyname(current_host)
     
     converter_args = [
         "/code/json_to_yaml_converter.py", '/opt/ml/input/config/hyperparameters.json'
@@ -104,8 +101,8 @@ def main():
         "--include", args.include, "--device", args.device
     ]
     
-    print("Master IP address:", fetched_master_ip)
-    print("Local IP address:", fetched_local_ip)
+    print("Master Host:", master_host)
+    print("Current Host:", current_host)
     
     run_script(converter_args) if args.hyp == "Custom" else None
         
