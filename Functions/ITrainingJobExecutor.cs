fbc09fcfe9cd682e9cac1f2d916a6f9f8457cd3c
Jhonray Acojedo <jhonray.acojedo@cit.edu>
2024-04-29 12:43:05 +0800

    Add  TrainingJobExecutor

---
+++ Functions/ITrainingJobExecutor.cs
@@ -0,0 +1,16 @@
+﻿using Amazon.CloudWatchLogs;
+using Amazon.SageMaker.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LSC_Trainer.Functions
+{
+    public interface ITrainingJobExecutor
+    {
+        CreateTrainingJobRequest CreateTrainingRequest(string img_size, string batch_size, string epochs, string weights, string data, string hyperparameters, string patience, string workers, string optimizer, string device, string instanceCount);
+        Task InitiateTrainingJob(CreateTrainingJobRequest trainingRequest, AmazonCloudWatchLogsClient cloudWatchLogsClient);
+    }
+}
---
+++ Functions/TrainingJobExecutor.cs
@@ -0,0 +1,23 @@
+﻿using Amazon.CloudWatchLogs;
+using Amazon.SageMaker.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LSC_Trainer.Functions
+{
+    internal class TrainingJobExecutor : ITrainingJobExecutor
+    {
+        public CreateTrainingJobRequest CreateTrainingRequest(string img_size, string batch_size, string epochs, string weights, string data, string hyperparameters, string patience, string workers, string optimizer, string device, string instanceCount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task InitiateTrainingJob(CreateTrainingJobRequest trainingRequest, AmazonCloudWatchLogsClient cloudWatchLogsClient)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
