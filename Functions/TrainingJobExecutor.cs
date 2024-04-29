f813982d02e7a04df855939405dfe7be834323e7
Ra-Jay <rjyansonx@gmail.com>
2024-04-30 00:08:07 +0800

    update: transger CreateTrainingJobRequest to TrainingJobExecutor class

--- Functions/TrainingJobExecutor.cs
+++ Functions/TrainingJobExecutor.cs
@@ -1,4 +1,5 @@
 ﻿using Amazon.CloudWatchLogs;
+using Amazon.SageMaker;
 using Amazon.SageMaker.Model;
 using System;
 using System.Collections.Generic;
@@ -10,9 +11,106 @@ namespace LSC_Trainer.Functions
 {
     internal class TrainingJobExecutor : ITrainingJobExecutor
     {
-        public CreateTrainingJobRequest CreateTrainingRequest(string img_size, string batch_size, string epochs, string weights, string data, string hyperparameters, string patience, string workers, string optimizer, string device, string instanceCount)
+        private Utility utility = new Utility();
+        private CustomHyperParamsForm customHyperParamsForm;
+
+        public CreateTrainingJobRequest CreateTrainingRequest(string img_size, string batch_size, string epochs, string weights, string data, string hyperparameters, string patience, string workers, string optimizer, string device, string instanceCount, string selectedInstance,string CUSTOM_UPLOADS_URI, string DEFAULT_DATASET_URI, string trainingFolder, string validationFolder, string ECR_URI, string SAGEMAKER_INPUT_DATA_PATH, string SAGEMAKER_OUTPUT_DATA_PATH, string ROLE_ARN, string DESTINATION_URI, string trainingJobName)
         {
-            throw new NotImplementedException();
+            CreateTrainingJobRequest trainingRequest = new CreateTrainingJobRequest()
+            {
+                AlgorithmSpecification = new AlgorithmSpecification()
+                {
+                    TrainingInputMode = "File",
+                    TrainingImage = ECR_URI,
+                    ContainerEntrypoint = new List<string>() { "python3", "/code/train_and_export.py" },
+                    ContainerArguments = new List<string>()
+                    {
+                        "--img-size", img_size,
+                        "--batch", batch_size,
+                        "--epochs", epochs,
+                        "--weights", weights,
+                        "--data", SAGEMAKER_INPUT_DATA_PATH + "train/" + data,
+                        "--hyp", hyperparameters,
+                        "--project", SAGEMAKER_OUTPUT_DATA_PATH,
+                        "--name", "results",
+                        "--patience", patience,
+                        "--workers", workers,
+                        "--optimizer", optimizer,
+                        "--device", device,
+                        "--include", "onnx",
+                        "--nnodes", instanceCount
+                    }
+                },
+                RoleArn = ROLE_ARN,
+                OutputDataConfig = new OutputDataConfig()
+                {
+                    S3OutputPath = DESTINATION_URI
+                },
+                // Keep this true so that devs will be using spot instances from now on
+                EnableManagedSpotTraining = true,
+                ResourceConfig = new ResourceConfig()
+                {
+                    InstanceCount = int.Parse(instanceCount),
+                    // Update the instance type everytime you select an instance type
+                    InstanceType = TrainingInstanceType.FindValue(selectedInstance),
+                    VolumeSizeInGB = 12
+                },
+                TrainingJobName = trainingJobName,
+                StoppingCondition = new StoppingCondition()
+                {
+                    MaxRuntimeInSeconds = 14400,
+                    MaxWaitTimeInSeconds = 15000,
+                },
+                HyperParameters = hyperparameters != "Custom" ? new Dictionary<string, string>()
+                {
+                    {"img-size", img_size},
+                    {"batch-size", batch_size},
+                    {"epochs", epochs},
+                    {"weights", weights},
+                    {"hyp", hyperparameters},
+                    {"patience", patience},
+                    {"workers", workers},
+                    {"optimizer", optimizer},
+                    {"device", device},
+                    {"include", "onnx" }
+                }
+                : customHyperParamsForm.HyperParameters,
+                InputDataConfig = new List<Channel>(){
+                    new Channel()
+                    {
+                        ChannelName = "train",
+                        InputMode = TrainingInputMode.File,
+                        CompressionType = Amazon.SageMaker.CompressionType.None,
+                        RecordWrapperType = RecordWrapper.None,
+                        DataSource = new DataSource()
+                        {
+                            S3DataSource = new S3DataSource()
+                            {
+                                S3DataType = S3DataType.S3Prefix,
+                                S3Uri = (utility.HasCustomUploads(CUSTOM_UPLOADS_URI) ? CUSTOM_UPLOADS_URI : DEFAULT_DATASET_URI) + trainingFolder,
+                                S3DataDistributionType = S3DataDistribution.FullyReplicated
+                            }
+                        }
+                    },
+                    new Channel()
+                    {
+                        ChannelName = "val",
+                        InputMode = TrainingInputMode.File,
+                        CompressionType = Amazon.SageMaker.CompressionType.None,
+                        RecordWrapperType = RecordWrapper.None,
+                        DataSource = new DataSource()
+                        {
+                            S3DataSource = new S3DataSource()
+                            {
+                                S3DataType = S3DataType.S3Prefix,
+                                S3Uri = (utility.HasCustomUploads(CUSTOM_UPLOADS_URI) ? CUSTOM_UPLOADS_URI : DEFAULT_DATASET_URI) + validationFolder,
+                                S3DataDistributionType = S3DataDistribution.FullyReplicated
+                            }
+                        }
+                    }
+                }
+            };
+            return trainingRequest;
         }
 
         public Task InitiateTrainingJob(CreateTrainingJobRequest trainingRequest, AmazonCloudWatchLogsClient cloudWatchLogsClient)
