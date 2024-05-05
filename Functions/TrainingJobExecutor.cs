cb7cc76e0837349c0516773e21dc8b632675b1af
kerrlabajo <labajokerr5@gmail.com>
2024-05-05 18:39:45 +0800

    Remove unused `trainingFolder` and `validationFolder` args

--- Functions/TrainingJobExecutor.cs
+++ Functions/TrainingJobExecutor.cs
@@ -22,7 +22,7 @@ namespace LSC_Trainer.Functions
         {
             this.uiUpdater = uiUpdater;
         }
-        public CreateTrainingJobRequest CreateTrainingRequest(string img_size, string batch_size, string epochs, string weights, string data, string hyperparameters, string patience, string workers, string optimizer, string device, string instanceCount, string selectedInstance,string CUSTOM_UPLOADS_URI, string DEFAULT_DATASET_URI, string trainingFolder, string validationFolder, string ECR_URI, string SAGEMAKER_INPUT_DATA_PATH, string SAGEMAKER_OUTPUT_DATA_PATH, string ROLE_ARN, string DESTINATION_URI, string trainingJobName, CustomHyperParamsForm customHyperParamsForm)
+        public CreateTrainingJobRequest CreateTrainingRequest(string img_size, string batch_size, string epochs, string weights, string data, string hyperparameters, string patience, string workers, string optimizer, string device, string instanceCount, string selectedInstance,string CUSTOM_UPLOADS_URI, string DEFAULT_DATASET_URI, string ECR_URI, string SAGEMAKER_INPUT_DATA_PATH, string SAGEMAKER_OUTPUT_DATA_PATH, string ROLE_ARN, string DESTINATION_URI, string trainingJobName, CustomHyperParamsForm customHyperParamsForm)
         {
             CreateTrainingJobRequest trainingRequest = new CreateTrainingJobRequest()
             {
